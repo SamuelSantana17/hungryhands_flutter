69b79aeb50d927d18336f7f9205e7ed9c70da131
SamuelSantana17 <samuel.santana.966@my.csun.edu>
2023-02-09 21:54:22 -0800

    test

--- Hungry_Hands_Restaurant/lib/screens/home_screen.dart
+++ Hungry_Hands_Restaurant/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../palatte.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -6,6 +9,53 @@ class HomeScreen extends StatelessWidget {
     return Stack(children: [
       Scaffold(
         backgroundColor: Colors.white,
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SafeArea(
+            child: Column(children: [
+              Text(
+                "Hungry Hands",
+                style: GoogleFonts.lemon(textStyle: kHeaderText),
+              ),
+              Text(
+                "Restaurant Admin",
+                style: GoogleFonts.lemon(textStyle: kBodyText),
+              ),
+              SizedBox(height: 30),
+              Container(
+                height: 600,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(16)),
+                child: Row(
+                  children: [
+                    // Padding(padding: EdgeInsets.symmetric(horizontal: 10)
+                    // )
+                    Expanded(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: [Text("order")],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: [Text("order")],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: [Text("order")],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ),
       ),
     ]);
   }

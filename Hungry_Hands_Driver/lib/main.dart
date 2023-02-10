9982916143af12ef029863ce662b0d22c46becba
SamuelSantana17 <samuel.santana.966@my.csun.edu>
2023-02-09 21:46:11 -0800

    test

--- Hungry_Hands_Driver/lib/main.dart
+++ Hungry_Hands_Driver/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         home: SplashScreen());
